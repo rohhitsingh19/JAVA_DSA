cb11b5ca18564315ff668158a561fc9112272020
ROHIT <154086284+rohhitsingh19@users.noreply.github.com>
2025-06-04 23:07:39 +0530

    Time: 12 ms (40.74%) | Memory: 45.6 MB (42.59%) - LeetSync

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String answerString(String word, int numFriends) {
+        int n = word.length();
+        if (numFriends == 1) {
+            return word;
+        }
+
+        int longestPossible = n - (numFriends - 1);
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            int canTakeLength = Math.min(longestPossible, n - i);
+            String sub = word.substring(i, i + canTakeLength);
+            if (sub.compareTo(result) > 0) {
+                result = sub;
+            }
+        }
+
+        return result;
+    }
+}
